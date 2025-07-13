34eac90b2972a0320aced852a4248939db8635e8
Oleg Bendiucov <punisher8585@gmail.com>
2025-07-13 08:05:45 +0300

    init: add Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -35,26 +35,11 @@ export default tseslint.config(
       'react-compiler/react-compiler': 'error',
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      'comma-dangle': ['error', 'only-multiline'],
-      'react/prop-types': 'off',
-      'react/display-name': 'off',
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/ban-ts-comment': 'error',
-      '@typescript-eslint/no-non-null-assertion': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      '@typescript-eslint/no-empty-function': 'off',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-var-requires': 'off',
-      'react/jsx-uses-react': 'off',
-      'react/react-in-jsx-scope': 'off',
-      'react-compiler/react-compiler': 'error',
     },
     settings: {
       react: {
         version: 'detect',
       },
     },
-  }
+  },
 );
