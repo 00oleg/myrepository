b3926b04adecb26a901f9afd843c7ee36ec1fa55
Oleg Bendiucov <punisher8585@gmail.com>
2025-08-17 19:31:34 +0300

    refactor: change next config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  distDir: './dist',
   poweredByHeader: false,
   reactStrictMode: true,
   trailingSlash: false,
