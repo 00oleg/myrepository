eecacc529e7629252898fb6694de3e9d3f0f9c56
Oleg Bendiucov <punisher8585@gmail.com>
2025-08-17 19:03:59 +0300

    feat: configure project for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  distDir: './dist',
   poweredByHeader: false,
   reactStrictMode: true,
   trailingSlash: false,
+
   async headers() {
     return [
       {
